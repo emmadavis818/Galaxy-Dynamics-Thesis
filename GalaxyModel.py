7ee7ac8e73bf1d5009c1bf7ad6bdb05629af14d8
emmadavis818 <124747582+emmadavis818@users.noreply.github.com>
2025-02-03 20:28:34 -0500

    Add files via upload

---
+++ GalaxyModel.py
@@ -0,0 +1,43 @@
+# %%
+import galaxykick as Galaxy
+
+# Third-party
+import astropy.units as u
+import pickle 
+import datetime
+y = datetime.datetime.now()
+print(y)
+
+# Gala
+import gala.dynamics as gd
+import gala.potential as gp
+from gala.units import galactic
+directory = "Feb3/"
+filename = "v10_225_p5_n1_dt01"
+print(filename)
+
+# %%
+orbitdict = {}
+orbitcount = 0
+maxIter = 500
+
+# %%
+noiselevel = 0.1
+print('noise level is ',noiselevel)
+w0 = gd.PhaseSpacePosition(pos=[5., 0., 0.]*u.kpc,vel=[10, 225, 0.]*u.km/u.s)
+
+# %%
+while orbitcount < maxIter:
+    print(f'orbit count = {orbitcount}')
+    newpot = Galaxy.MWwNoise(N=noiselevel,units=galactic)
+    neworbit = newpot.integrate_orbit(w0, dt=0.01 * u.Myr, n_steps=10000)
+    orbitdict[str(noiselevel)+'_'+str(orbitcount)] = neworbit
+    orbitcount += 1
+
+# %%
+with open(directory+filename+'.pkl', 'wb') as f:
+    pickle.dump(orbitdict, f)
+
+# %%
+x = datetime.datetime.now()
+print(x-y)
