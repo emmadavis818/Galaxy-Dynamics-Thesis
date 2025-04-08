bb0f1d393cc122e634fa875c3bc856120f6ce09f
emmadavis818 <124747582+emmadavis818@users.noreply.github.com>
2025-04-08 12:37:55 -0400

    Update galaxykick.py

--- galaxykick.py
+++ galaxykick.py
@@ -5,18 +5,19 @@ mwp = gp.MilkyWayPotential2022()
 
 class MWwNoise(gp.PotentialBase):
     N = gp.PotentialParameter("N")
+    dt = gp.PotentialParameter("dt")
     ndim = 3
     
     def _energy(self, xyz, t):
-        # N = self.parameters['N'].value
         xyz = xyz.T
         energy = mwp.energy(xyz)
         return energy/energy.unit
 
     def _gradient(self, xyz, t):
         N = self.parameters['N'].value
+        dt = self.parameters['dt'].value
         xyz = xyz.T
         grad = (mwp.gradient(xyz)).T
-        noise = N*(np.random.rand(1,3)-0.5) * grad.unit
+        noise = (N/dt)*(np.random.rand(1,3)-0.5)*[1,1,0]* grad.unit
         gradwnoise = grad + noise
         return gradwnoise/gradwnoise.unit
